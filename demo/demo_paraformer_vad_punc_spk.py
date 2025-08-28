9b2193dd2395c3d6c796f311de3a04d0f847d3a8
boomytc <767478228@qq.com>
2025-08-28 15:38:17 +0800

    feat(demo): 添加funasr组合模型语音识别演示脚本

---
+++ demo/demo_paraformer_vad_punc_spk.py
@@ -0,0 +1,26 @@
+from funasr import AutoModel
+from contextlib import redirect_stderr, redirect_stdout
+from io import StringIO
+
+PARAFORMER_MODEL="models/asr_models/paraformer"
+VAD_MODEL="models/vad_models/fsmn_vad"
+PUNC_MODEL="models/punc_models/punc_ct"
+SPK_MODEL="models/spk_models/campplus_sv"
+
+with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
+    model = AutoModel(
+        model=PARAFORMER_MODEL,
+        vad_model=VAD_MODEL,
+        vad_kwargs={"max_single_segment_time": 60000},
+        punc_model=PUNC_MODEL,
+        spk_model=SPK_MODEL,
+        disable_update=True,
+    )
+
+with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
+    res = model.generate(
+        input = "/Users/boom/Music/乐飞航空/manual/data/ytc/manual171_0113.wav",
+        cache={},
+    )
+
+print(res)
