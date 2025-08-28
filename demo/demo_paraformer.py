74f16d8dea57b35173a6f806c3d24909da483041
boomytc <767478228@qq.com>
2025-08-28 15:38:00 +0800

    refactor(demo): 简化demo_paraformer.py脚本并重定向输出
    移除硬编码的模型路径和特定语言的配置，使用统一模型路径
    添加输出重定向以捕获模型日志
    更新输入文件路径为更通用的示例

--- demo/demo_paraformer.py
+++ demo/demo_paraformer.py
@@ -1,50 +1,19 @@
 from funasr import AutoModel
+from contextlib import redirect_stderr, redirect_stdout
+from io import StringIO
 
-PARAFORMER_ZH_MODEL="/home/lf/Model/ASR/paraformer_models/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
-PARAFORMER_EN_MODEL="/home/lf/Model/ASR/paraformer_models/speech_paraformer_asr-en-16k-vocab4199-pytorch"
-VAD_MODEL="/home/lf/Model/ASR/vad_models/speech_fsmn_vad_zh-cn-16k-common-pytorch"
-PUNC_MODEL="/home/lf/Model/ASR/punc_models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch"
-SPK_MODEL="/home/lf/Model/ASR/spk_models/speech_campplus_sv_zh-cn_16k-common"
+PARAFORMER_MODEL="models/asr_models/paraformer"
 
-# asr_model=PARAFORMER_ZH_MODEL
-asr_model=PARAFORMER_EN_MODEL
-
-model = AutoModel(
-    model=asr_model,
-    vad_model=VAD_MODEL,
-    vad_kwargs={"max_single_segment_time": 60000},
-    punc_model=PUNC_MODEL,
-    spk_model=SPK_MODEL,
-    disable_update=True,
-)
-
-if asr_model == PARAFORMER_ZH_MODEL:
-    res = model.generate(
-        input = "/home/lf/音乐/诗朗诵_面朝大海春暖花开.wav",
-        cache={},
+with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
+    model = AutoModel(
+        model=PARAFORMER_MODEL,
+        disable_update=True,
     )
-elif asr_model == PARAFORMER_EN_MODEL:
+
+with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
     res = model.generate(
-        input = "/home/lf/音乐/asr_example_en.wav",
+        input = "/Users/boom/Music/乐飞航空/manual/data/ytc/manual171_0113.wav",
         cache={},
-        pred_timestamp=True,
-        return_raw_text=True,
-        sentence_timestamp=True,
-        en_post_proc=True,
     )
 
 print(res)
-
-
-""" 像下面一样调用英语模型来得到详细的时间戳
-选择英语模型speech_paraformer_asr-en-16k-vocab4199-pytorch
- res = model.generate(
-    input="",
-    cache={},
-    pred_timestamp=True,
-    return_raw_text=True,
-    sentence_timestamp=True,
-    en_post_proc=True,
-)
-
-"""
